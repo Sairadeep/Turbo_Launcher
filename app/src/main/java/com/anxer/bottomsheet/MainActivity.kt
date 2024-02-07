1cca780ff135637cbcaff3165b2a8266481b730e
Sai Pradeep Vamala <sai.vamala@wenable.com>
2024-02-07 16:34:07 +0530

    Added logic for displaying badge numbers for whatsapp and instagram

--- app/src/main/java/com/anxer/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/anxer/bottomsheet/MainActivity.kt
@@ -271,7 +271,6 @@ fun BottomSheetDemo() {
                                     )
                                 }
                             }
-
                         }
                     )
                 }
